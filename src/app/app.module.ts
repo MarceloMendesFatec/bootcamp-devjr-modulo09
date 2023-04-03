40596873dbb2667e796f1bbd84709c1c024c4c48
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2023-04-03 18:19:50 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    TableComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
